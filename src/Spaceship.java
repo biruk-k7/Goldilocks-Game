daa60bdfb0b2a3042529f71ce12593201d7bc51d
bilaltiq <mtariq27@amherst.edu>
2024-04-26 18:24:29 -0400

    Spaceship edits

--- src/Spaceship.java
+++ src/Spaceship.java
@@ -1,5 +1,28 @@
-//package Final;
 
 public class Spaceship {
+
+    private int fuel;
+    private int level;
     
+
+    public Spaceship(){
+        this.fuel = 10;
+        this.level = 1;
+    }
+
+    public void upgrade(){
+
+    }
+
+    public void refuel(){
+
+    }
+    
+    public int getFuel(){
+        return fuel;
+    }
+
+    public int getLevel(){
+        return level;
+    }
 }
